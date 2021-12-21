139ff81891cab17fccef000d9db0053bf4d321f8
sduvapu <81993734+sduvapu@users.noreply.github.com>
2021-12-21 16:13:13 -0500

    Create mult.py

---
+++ calculator/calc/mult.py
@@ -0,0 +1,12 @@
+"""Multiplication Class"""
+from calculator.calc.calc import Calculation
+
+
+class Multiplication(Calculation):
+    """subtraction calculation object"""
+    def get_result(self):
+        """get the multiplication results"""
+        result = 1.0
+        for value in self.values:
+            result = result * value
+        return result
